7114e4f5b8f5283ae579a0ec75d253cf4062f43a
HotariTobu <hotari24tools@gmail.com>
2023-08-24 07:36:02 +0900

    add: array C acquired ref

--- src/components/my/Array.vue
+++ src/components/my/Array.vue
@@ -4,8 +4,12 @@ import { reactive, ref } from 'vue';
 const A = ref(['a', 'b', 'c',])
 const B = reactive(['a', 'b', 'c',])
 
+const C = ref(null)
+C.value = ['a', 'b', 'c',]
+
 const av = ref('')
 const bv = ref('')
+const cv = ref('')
 
 const addA = () => {
     A.value.push(av.value)
@@ -17,6 +21,11 @@ const addB = () => {
     bv.value = ''
 }
 
+const addC = () => {
+    C.value.push(cv.value)
+    cv.value = ''
+}
+
 const removeA = () => {
     A.value.splice(av.value, 1)
     av.value = ''
@@ -25,6 +34,11 @@ const removeA = () => {
 const removeB = () => {
     B.splice(bv.value, 1)
     bv.value = ''
+}
+
+const removeC = () => {
+    C.value.splice(cv.value, 1)
+    cv.value = ''
 }
 </script>
 
@@ -45,6 +59,13 @@ const removeB = () => {
             <input v-model="bv">
             <button @click="addB">add</button>
             <button @click="removeB">remove</button>
+        </div>
+        <div>
+            <h4>acquired ref</h4>
+            <p v-for="v in C" :key="v">{{ v }}</p>
+            <input v-model="cv">
+            <button @click="addC">add</button>
+            <button @click="removeC">remove</button>
         </div>
     </div>
 </template>
